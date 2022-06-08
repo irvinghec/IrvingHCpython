c124d2883e62c3349a87ab1fa9bfcdfe85761f89
irvinghec <99280243+irvinghec@users.noreply.github.com>
2022-06-08 14:05:31 -0500

    Update basedatos.sql

--- basedatos.sql
+++ basedatos.sql
@@ -1,23 +1,23 @@
-CREATE TABLE IF NO EXISTS ihc_python;
+CREATE DATABASE IF NOT EXISTS ihc_python;
 USE ihc_python;
 
 CREATE TABLE usuarios(
-    id          int(25) auto_imcrement not null,
+    id          int(25) auto_increment NOT NULL,
     nombre      varchar(100),
     apellidos   varchar(255),
-    email       varchar(255) not null,
-    password    varchar(255) not null,
-    fecha       date not null,
+    email       varchar(255) NOT NULL,
+    password    varchar(255) NOT NULL,
+    fecha       date NOT NULL,
     CONSTRAINT pk_usuarios PRIMARY KEY(id),
     CONSTRAINT uq_email UNIQUE(email)
 )ENGINE=InnoDb;
 
 CREATE TABLE notas(
-    id int(25) auto_imcrement not null,
-    usuario_id int(25) not null,
-    titulo varchar(255) not null,
+    id int(25) auto_increment NOT NULL,
+    usuario_id int(25) NOT NULL,
+    titulo varchar(255) NOT NULL,
     descripcion MEDIUMTEXT,
-    fecha date not null,
+    fecha date NOT NULL,
     CONSTRAINT pk_notas PRIMARY KEY(id),
     CONSTRAINT fk_notas_usuario FOREIGN KEY(usuario_id) REFERENCES usuarios(id)
 )ENGINE=InnoDb;
